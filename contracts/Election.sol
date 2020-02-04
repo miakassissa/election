4c405587354ea30b91b4db8aad39e07658856c5c
miakassissa <bgmiakassiss@gmail.com>
2020-02-04 05:45:18 +0100

    commit 3
    commit 3 - solc versions

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.6.1;     // Suitable Version for running in Visual Studio Code
+// pragma solidity ^0.5.16; // Suitable Version for running in the CMD
 
 contract Election {
     // Stocker Candidat
