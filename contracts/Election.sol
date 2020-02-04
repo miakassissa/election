2d7f9b9b5f5af8b8e24ff538c8e1e4ebbbef4332
miakassissa <bgmiakassiss@gmail.com>
2020-02-04 05:07:42 +0100

    initial commit
    Initial Commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+    // Stocker Candidat
+    // Lire Candidat
+    string public candidat;
+    // Constructor
+    constructor() public {
+        candidat = "Candidat 1";
+    }
+
+}
