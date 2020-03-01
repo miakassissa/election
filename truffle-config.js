462c80f613afa455e566baae9322a57ab690dfda
miakassissa <bgmiakassiss@gmail.com>
2020-03-01 02:44:39 +0100

    Successful Deployement Ropsten
    Changes To Successful Deployement To The Ropsten Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config()  // Store environment-specific variable from '.env' to process.env
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,8 +10,25 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MTMSK_MNEMONIC,
+          "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY
+        );
+      },
+      network_id: 3,
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
     develop: {
       port: 8545
+    },
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
 };
