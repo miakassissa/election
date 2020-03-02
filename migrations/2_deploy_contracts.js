649164241fd558f0281babc21357a6ccfb07e81c
miakassissa <bgmiakassiss@gmail.com>
2020-03-02 19:15:06 +0100

    re-Initial Commit
    truffle unbox webpack

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
-var Election = artifacts.require("./Election.sol");
+const ConvertLib = artifacts.require("ConvertLib");
+const MetaCoin = artifacts.require("MetaCoin");
 
-module.exports = function (deployer) {
-    deployer.deploy(Election);
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
 };
